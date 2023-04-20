c76a058bd3623716360602164d1683a9f79638d8
RANIT SARKAR <85814394+RANIT001@users.noreply.github.com>
2023-04-20 07:01:18 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# from tkinter import *
+# # _______________lib_______________
+# win = Tk()
+# #getting screen width and height of display
+# width= win.winfo_screenwidth()
+# height= win.winfo_screenheight()
+# #setting tkinter window size
+# win.geometry("%dx%d" % (width, height))
+# win.title("Medi_rec")
+# intro = Label(text="Medi-place")
+#
+#
+# intro.grid(row=0,column=0,columnspan=3)
+#
+# win.mainloop()
+
